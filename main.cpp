3e74932063cc25802d2de098cb0068a7028b3306
cloudqq <cloudqq@gmail.com>
2011-08-07 14:47:44 +0800

    qnote init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2011年08月07日 14时29分46秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mario (), cloudqq@gmail.com
+ *        Company:  chinadim
+ *
+ * =====================================================================================
+ */
+
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+
+	return app.exec();
+}
+
---
+++ qnote.pro
@@ -0,0 +1,8 @@
+SOURCES = main.cpp
+
+target.path 		= qnote
+sources.files 		= $$SOURCES $$HEADS $$RESOURCES $$FORMS qnote.pro README
+sources.path 		= qnote
+
+INSTALLS += target sources
+
